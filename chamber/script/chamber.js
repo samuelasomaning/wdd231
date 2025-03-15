09b5146e1902214e888448aca4dc7dee57b38349
samuelasomaning <samuelscorpa87@gmail.com>
2025-03-15 22:10:48 +0000

    chamber of commerce update

---
+++ chamber/script/chamber.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("year").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = document.lastModified;
+
+    //display members
+    async function fetchMembers() {
+        const response = await fetch("data/members.json");
+        const data = await response.json();
+        displayMembers(data.members);
+    }
+
+    function displayMembers(members) {
+        const container = document.getElementById("members-container");
+
+        members.forEach(member => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+
+            card.innerHTML = `
+                <img src = "${member.img}" alt="${member.name}">
+                <h2>${member.name}</h2>
+                <p>${member.address}</p>
+                <p>${member.phone}</p>
+                <a href="${member.website}" target="_blank">Visit Website</a>
+            `;
+            container.appendChild(card);
+        });
+    }
+
+    fetchMembers();
+
+    document.getElementById("grid-view").addEventListener("click", () => {
+        document.getElementById("members-container").classList.remove("list");
+    });
+
+    document.getElementById("list-view").addEventListener("click", () => {
+        document.getElementById("members-container").classList.add("list");
+    });
+
+});
